9070f9a7fcdb19d74852024614acfceab3eb3616
hamer-sniper <wm-andrew@mail.ru>
2021-10-08 10:01:31 +0300

    Вкладка добавлена, не корректно

--- Site open WinForms/MainForm.Designer.cs
+++ Site open WinForms/MainForm.Designer.cs
@@ -34,13 +34,19 @@
             this.dvelopInfo = new System.Windows.Forms.Button();
             this.knowledge = new System.Windows.Forms.Button();
             this.service = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabMain = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // KS_Open
             // 
-            this.KS_Open.Location = new System.Drawing.Point(72, 53);
+            this.KS_Open.Location = new System.Drawing.Point(281, 91);
+            this.KS_Open.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.KS_Open.Name = "KS_Open";
-            this.KS_Open.Size = new System.Drawing.Size(130, 23);
+            this.KS_Open.Size = new System.Drawing.Size(412, 65);
             this.KS_Open.TabIndex = 0;
             this.KS_Open.Text = "Открыть RX";
             this.KS_Open.UseVisualStyleBackColor = true;
@@ -48,9 +54,10 @@
             // 
             // webInfo
             // 
-            this.webInfo.Location = new System.Drawing.Point(72, 101);
+            this.webInfo.Location = new System.Drawing.Point(281, 220);
+            this.webInfo.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.webInfo.Name = "webInfo";
-            this.webInfo.Size = new System.Drawing.Size(130, 23);
+            this.webInfo.Size = new System.Drawing.Size(412, 65);
             this.webInfo.TabIndex = 1;
             this.webInfo.Text = "Справка Веб-Клиент";
             this.webInfo.UseVisualStyleBackColor = true;
@@ -58,9 +65,10 @@
             // 
             // dvelopInfo
             // 
-            this.dvelopInfo.Location = new System.Drawing.Point(72, 149);
+            this.dvelopInfo.Location = new System.Drawing.Point(281, 341);
+            this.dvelopInfo.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.dvelopInfo.Name = "dvelopInfo";
-            this.dvelopInfo.Size = new System.Drawing.Size(130, 23);
+            this.dvelopInfo.Size = new System.Drawing.Size(412, 65);
             this.dvelopInfo.TabIndex = 2;
             this.dvelopInfo.Text = "Справка разработчик";
             this.dvelopInfo.UseVisualStyleBackColor = true;
@@ -68,9 +76,10 @@
             // 
             // knowledge
             // 
-            this.knowledge.Location = new System.Drawing.Point(72, 197);
+            this.knowledge.Location = new System.Drawing.Point(281, 474);
+            this.knowledge.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.knowledge.Name = "knowledge";
-            this.knowledge.Size = new System.Drawing.Size(130, 23);
+            this.knowledge.Size = new System.Drawing.Size(412, 65);
             this.knowledge.TabIndex = 3;
             this.knowledge.Text = "База знаний";
             this.knowledge.UseVisualStyleBackColor = true;
@@ -78,27 +87,65 @@
             // 
             // service
             // 
-            this.service.Location = new System.Drawing.Point(72, 248);
+            this.service.Location = new System.Drawing.Point(281, 595);
+            this.service.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.service.Name = "service";
-            this.service.Size = new System.Drawing.Size(130, 23);
+            this.service.Size = new System.Drawing.Size(412, 65);
             this.service.TabIndex = 4;
             this.service.Text = "Тех поддержка";
             this.service.UseVisualStyleBackColor = true;
             this.service.Click += new System.EventHandler(this.service_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabMain);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1078, 890);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabMain
+            // 
+            this.tabMain.Controls.Add(this.service);
+            this.tabMain.Controls.Add(this.KS_Open);
+            this.tabMain.Controls.Add(this.knowledge);
+            this.tabMain.Controls.Add(this.webInfo);
+            this.tabMain.Controls.Add(this.dvelopInfo);
+            this.tabMain.Location = new System.Drawing.Point(12, 58);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMain.Size = new System.Drawing.Size(1054, 820);
+            this.tabMain.TabIndex = 0;
+            this.tabMain.Text = "Основное";
+            this.tabMain.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(12, 58);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1054, 820);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(277, 321);
-            this.Controls.Add(this.service);
-            this.Controls.Add(this.knowledge);
-            this.Controls.Add(this.dvelopInfo);
-            this.Controls.Add(this.webInfo);
-            this.Controls.Add(this.KS_Open);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1102, 914);
+            this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Открытие рабочих сайтов";
+            this.tabControl1.ResumeLayout(false);
+            this.tabMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -110,6 +157,9 @@
         private System.Windows.Forms.Button dvelopInfo;
         private System.Windows.Forms.Button knowledge;
         private System.Windows.Forms.Button service;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabMain;
     }
 }
 
--- Site open WinForms/Properties/AssemblyInfo.cs
+++ Site open WinForms/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,12 +7,12 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
 [assembly: AssemblyTitle("Site open WinForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Открытие сайтов связанных с DirectumRX")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andrew")]
 [assembly: AssemblyProduct("Site open WinForms")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Andrew ™ 2021")]
 [assembly: AssemblyCulture("")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
